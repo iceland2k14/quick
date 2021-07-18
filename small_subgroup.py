e4312eac3e944004549bf9d97657f2e0b8199de5
iceland <75991805+iceland2k14@users.noreply.github.com>
2021-07-18 14:17:31 +0530

    #1 Linux. With Endo & Sym.

--- small_subgroup.py
+++ small_subgroup.py
@@ -5,28 +5,33 @@ Usage :
  
 @author: iceland
 """
-# import gmpy2
-# from fastecdsa import curve
-# from fastecdsa.point import Point
-# import bit
+import os, platform, sys
 import random
 import ctypes
+
+verbosity = False
 # =============================================================================
-# G = curve.secp256k1.G
-# N = curve.secp256k1.q
-# 
-# factor1 = 107361793816595537
-# factor2 = 174723607534414371449
-# factor3 = 341948486974166000522343609283189
-# k = factor1 * factor2 * factor3
-# 
-# btc_list = ['1PSRcasBNEwPC2TWUB68wvQZHwXy4yqPQ3',
-#             '1B5USZh6fc2hvw2yW9YaVF75sJLcLQ4wCt',
-#             '1EHNa6Q4Jz2uvNExL497mE43ikXhwF6kZm',
-#             '1JPbzbsAx1HyaDQoLMapWGoqf9pD5uha5m']
-# =============================================================================
-print('[+] Program Starting ... Please Wait')
-ice = ctypes.CDLL('ice_secp256k1.dll')
+print('\n[+] Program Starting ... Please Wait')
+if platform.system().lower().startswith('win'):
+    dllfile = 'ice_secp256k1.dll'
+    if os.path.isfile(dllfile) == True:
+        pathdll = os.path.realpath(dllfile)
+        ice = ctypes.CDLL(pathdll)
+    else:
+        print('File {} not found'.format(dllfile))
+    
+elif platform.system().lower().startswith('lin'):
+    dllfile = 'ice_secp256k1.so'
+    if os.path.isfile(dllfile) == True:
+        pathdll = os.path.realpath(dllfile)
+        ice = ctypes.CDLL(pathdll)
+    else:
+        print('File {} not found'.format(dllfile))
+    
+else:
+    print('[-] Unsupported Platform currently for ctypes dll method. Only [Windows and Linux] is working')
+    sys.exit()
+
 ice.privatekey_to_address.argtypes = [ctypes.c_int, ctypes.c_bool, ctypes.c_char_p]  # 012,comp,pvk
 ice.privatekey_to_address.restype = ctypes.c_char_p
 ice.init_secp256_lib()
@@ -51,37 +56,106 @@ segwit_btc_list = set(segwit_btc_list)
 bech32_btc_list = set(bech32_btc_list)
 
 N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+lmda = 0x5363ad4cc05c30e0a5261c028812645a122e22ea20816678df02967c1b23bd72
+lmda2 = 0xac9c52b33fa3cf1f5ad9e3fd77ed9ba4a880b9fc8ec739c2e0cfc810b51283ce      # lmda*lmda
 ###############################################################################
 
 while True:
-    a_largebit_number = random.randint(2**225, 2**235)
+    a_largebit_number = random.SystemRandom().randint(2**220, 2**240)
     # k = gmpy2.next_prime(a_largebit_number)    # factor1 * factor2 * factor3
     k = a_largebit_number
-    print('\n[+] Starting search for the subgroup :', hex(k))
+    print('\n[+] Starting search for the subgroup : {}     [{} bit]'.format(hex(k),len(bin(k)[2:])))
     
     for m in range(1, N//k):
         small_subgroup_pvk = m * k
-        addrc = privatekey_to_address(ctypes.c_int(0), True, small_subgroup_pvk)
-        addru = privatekey_to_address(0, False, small_subgroup_pvk)
-        addr3 = privatekey_to_address(1, True, small_subgroup_pvk)
-        addrb = privatekey_to_address(2, True, small_subgroup_pvk)
+        pln = small_subgroup_pvk*lmda%N
+        pl2n = small_subgroup_pvk*lmda2%N
+        np = N-small_subgroup_pvk
+        npln = N-small_subgroup_pvk*lmda%N
+        npl2n = N-small_subgroup_pvk*lmda2%N
+        
+        addrc1 = privatekey_to_address(0, True, small_subgroup_pvk)
+        addru1 = privatekey_to_address(0, False, small_subgroup_pvk)
+        addr31 = privatekey_to_address(1, True, small_subgroup_pvk)
+        addrb1 = privatekey_to_address(2, True, small_subgroup_pvk)
+        
+        addrc2 = privatekey_to_address(0, True, pln)
+        addru2 = privatekey_to_address(0, False, pln)
+        addr32 = privatekey_to_address(1, True, pln)
+        addrb2 = privatekey_to_address(2, True, pln)
+        
+        addrc3 = privatekey_to_address(0, True, pl2n)
+        addru3 = privatekey_to_address(0, False, pl2n)
+        addr33 = privatekey_to_address(1, True, pl2n)
+        addrb3 = privatekey_to_address(2, True, pl2n)
+        
+        addrc4 = privatekey_to_address(0, True, np)
+        addru4 = privatekey_to_address(0, False, np)
+        addr34 = privatekey_to_address(1, True, np)
+        addrb4 = privatekey_to_address(2, True, np)
+        
+        addrc5 = privatekey_to_address(0, True, npln)
+        addru5 = privatekey_to_address(0, False, npln)
+        addr35 = privatekey_to_address(1, True, npln)
+        addrb5 = privatekey_to_address(2, True, npln)
+        
+        addrc6 = privatekey_to_address(0, True, npl2n)
+        addru6 = privatekey_to_address(0, False, npl2n)
+        addr36 = privatekey_to_address(1, True, npl2n)
+        addrb6 = privatekey_to_address(2, True, npl2n)
 
-        if addrc in legacy_btc_list or addru in legacy_btc_list or addr3 in segwit_btc_list or addrb in bech32_btc_list:
+        if addrc1 in legacy_btc_list or addru1 in legacy_btc_list or addr31 in segwit_btc_list or addrb1 in bech32_btc_list:
             print('============== KEYFOUND ==============')
             print('PrivateKey :', hex(small_subgroup_pvk) )
-            print('[C]',addrc,'[U]',addru)
-            print('[3]',addr3,'[bc]',addrb)
+            print('[C]',addrc1,'[U]',addru1)
+            print('[3]',addr31,'[bc]',addrb1)
+            print('======================================')
+            
+        if addrc2 in legacy_btc_list or addru2 in legacy_btc_list or addr32 in segwit_btc_list or addrb2 in bech32_btc_list:
+            print('============== KEYFOUND ==============')
+            print('PrivateKey :', hex(pln) )
+            print('[C]',addrc2,'[U]',addru2)
+            print('[3]',addr32,'[bc]',addrb2)
+            print('======================================')
+        
+        if addrc3 in legacy_btc_list or addru3 in legacy_btc_list or addr33 in segwit_btc_list or addrb3 in bech32_btc_list:
+            print('============== KEYFOUND ==============')
+            print('PrivateKey :', hex(pl2n) )
+            print('[C]',addrc3,'[U]',addru3)
+            print('[3]',addr33,'[bc]',addrb3)
+            print('======================================')
+            
+        if addrc4 in legacy_btc_list or addru4 in legacy_btc_list or addr34 in segwit_btc_list or addrb4 in bech32_btc_list:
+            print('============== KEYFOUND ==============')
+            print('PrivateKey :', hex(np) )
+            print('[C]',addrc4,'[U]',addru4)
+            print('[3]',addr34,'[bc]',addrb4)
+            print('======================================')
+            
+        if addrc5 in legacy_btc_list or addru5 in legacy_btc_list or addr35 in segwit_btc_list or addrb5 in bech32_btc_list:
+            print('============== KEYFOUND ==============')
+            print('PrivateKey :', hex(npln) )
+            print('[C]',addrc5,'[U]',addru5)
+            print('[3]',addr35,'[bc]',addrb5)
+            print('======================================')
+            
+        if addrc6 in legacy_btc_list or addru6 in legacy_btc_list or addr36 in segwit_btc_list or addrb6 in bech32_btc_list:
+            print('============== KEYFOUND ==============')
+            print('PrivateKey :', hex(npl2n) )
+            print('[C]',addrc6,'[U]',addru6)
+            print('[3]',addr36,'[bc]',addrb6)
             print('======================================')
 
-        if m%10000 == 0: print('[+] Completed : ', m,'[C]',addrc,'[U]',addru, '[3]',addr3,'[bc]',addrb, end='\r')
+        if m%10000 == 0: 
+            if verbosity is True: print('[+] Completed : ', 6*m,\
+                  '[C-1]',addrc1,'[U-1]',addru1, '[3-1]',addr31,'[bc-1]',addrb1, \
+                  '[C-2]',addrc2,'[U-2]',addru2, '[3-2]',addr32,'[bc-2]',addrb2, \
+                  '[C-3]',addrc3,'[U-3]',addru3, '[3-3]',addr33,'[bc-3]',addrb3, \
+                  '[C-4]',addrc4,'[U-4]',addru4, '[3-4]',addr34,'[bc-4]',addrb4, \
+                  '[C-5]',addrc5,'[U-5]',addru5, '[3-5]',addr35,'[bc-5]',addrb5, \
+                  '[C-6]',addrc6,'[U-6]',addru6, '[3-6]',addr36,'[bc-6]',addrb6, \
+                  end='\r')
+            else: print('[+] Completed : ', 6*m, end='\r')
     print('', end='\r')
-# =============================================================================
-# def factors(n):
-#     result = set()
-#     n = gmpy2.mpz(n)
-#     for i in range(1, gmpy2.isqrt(n) + 1):
-#         div, mod = divmod(n, i)
-#         if not mod:
-#             result |= {gmpy2.mpz(i), div}
-#     return result
-# =============================================================================
+    
+###############################################################################
