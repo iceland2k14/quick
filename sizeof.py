6fb0ebad7b734c0396589b1be44a8b2d134c8f4d
iceland <75991805+iceland2k14@users.noreply.github.com>
2022-03-11 19:57:45 +0530

    Create sizeof.py

---
+++ sizeof.py
@@ -0,0 +1,9 @@
+# estimate the size of any Python Object in RAM
+def sizeof(obj):
+    size = sys.getsizeof(obj)
+    if isinstance(obj, dict): return size + sum(map(sizeof, obj.keys())) + sum(map(sizeof, obj.values()))
+    if isinstance(obj, (list, tuple, set, frozenset)): return size + sum(map(sizeof, obj))
+    return size
+
+x = [3, 7, 277]
+print(sizeof(x))
