ef262b3be415a98b968273c55f113106ca3954de
pianist-coder <101991255+pianist-coder@users.noreply.github.com>
2024-06-07 19:49:17 +0300

    Update PubDiv.py
    Code optimization and cleaning..

--- PubDiv.py
+++ PubDiv.py
@@ -1,67 +1,49 @@
-# -*- coding: utf-8 -*-
-"""
-Usage :
- > python PubDiv.py 02CEB6CBBCDBDF5EF7150682150F4CE2C6F4807B349827DCDBDD1F2EFA885A2630 5 keydiv_bit5.txt
+#-------------------------------------------------------------------------
+# Name:        PubDiv
+# Author:      iceland
+# Created:     07.06.2024
+# Usage:       python PubDiv.py 02CEB6CBBCDBDF5EF7150682150F4CE2C6F4807B349827DCDBDD1F2EFA885A2630 15 keydiv_bit5.txt
+#-------------------------------------------------------------------------
 
-@author: iceland
-"""
 from time import time
 import os
 import sys
-from fastecdsa import curve
-from fastecdsa.point import Point
-import bit
+import secp256k1 as ice
 
-G = curve.secp256k1.G
-N = curve.secp256k1.q
+#=========================================================================
 
-
-if len(sys.argv) > 4 or len(sys.argv) < 4:
-    print('[+] Program Usage.... ')
-    print('{} <original_pubkey> <bit_reduction> <output_file_name>\n'.format(sys.argv[0]))
-    print('Example to create a text File [upub]:\n{} 02CEB6CBBCDBDF5EF7150682150F4CE2C6F4807B349827DCDBDD1F2EFA885A2630 5 keydiv_bit5.txt'.format(sys.argv[0]))
-    sys.exit()
-
-
-def pub2point(pub_hex):
- x = int(pub_hex[2:66],16)
- if len(pub_hex) < 70:
-  y = bit.format.x_to_y(x, int(pub_hex[:2],16)%2)
- else:
-  y = int(pub_hex[66:], 16)
- return Point(x,y,curve=curve.secp256k1)
-
-def point2upub(A):
-    return '04'+ hex(A.x)[2:].zfill(64) + hex(A.y)[2:].zfill(64)
-    
-def upub2cpub(upub_bytes):
-    x1 = upub_bytes[1:33]
-    prefix = str(2 + int(upub_bytes[33:].hex(), 16)%2).zfill(2)
-    return bytes.fromhex(prefix)+x1
-###############################################################################
 pubkey = sys.argv[1]
 nbits = int(sys.argv[2])
 out_file = sys.argv[3]
 
-total_keys = 2**nbits
-print('[+] Result will write Total {} pubkeys in the Output File size Approx. [{:.3f} MB]'.format(total_keys, total_keys*132/(1024*1024)))
+if len(sys.argv) > 4 or len(sys.argv) < 4:
+    print('[+] Program Usage.... ')
+    print(f'python {sys.argv[0]} <original_pubkey> <bit_reduction> <output_file_name>\n')
+    print(f'Example to create a text File:\n\npython {sys.argv[0]} 02CEB6CBBCDBDF5EF7150682150F4CE2C6F4807B349827DCDBDD1F2EFA885A2630 15 keydiv_bit5.txt\n')
+    sys.exit()
+    
+#=========================================================================
 
-Q = pub2point(pubkey)
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+total_keys = 2**nbits
+Q = ice.pub2upub(pubkey)
 
 # k = 1/total_keys
-k = pow(total_keys, N-2, N)
-
+k = pow(total_keys, N - 2, N)
 st = time()
-###############################################################################
 out = open(out_file, 'w')
 
+#=========================================================================
+print(f'[+] Result will write Total {total_keys} pubkeys in the Output File size Approx. [{total_keys*132/(1024*1024):.3f} MB]')
+#=========================================================================
+
 for i in range(total_keys):
-    P = Q - (i*G)
-    out.write(point2upub(k*P)+'\n')
-    if i%100 == 0: print('[+] Completed : ', i, end='\r')
+    P = ice.point_subtraction(Q, ice.scalar_multiplication(i))
+    out.write(f'{ice.point_multiplication(P, k).hex()}\n')
+    if i % 1000 == 0: print(f'[+] Completed: {i}', end='\r')
     
 out.flush()
 os.fsync(out.fileno())
 out.close()
-print('[+] Finished Total DivKeys # ', total_keys, end= '\n')
-print('[+] Completed in {0:.2f} sec'.format(time() - st))
+print(f'[+] Finished Total DivKeys: {total_keys}\n')
+print(f'[+] Completed in {time() - st:.2f} sec')
